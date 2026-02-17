42c6ce4a279012ae313825051367f58c8c80913a
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2026-02-17 12:35:02 +0100

    Added posters, contact and cart

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const buttonElement = document.getElementById("add-to-cart-btn");
+
+buttonElement.addEventListener("click", function (event) {
+  alert("Item added to cart!");
+  console.log("Item added to cart!");
+});
