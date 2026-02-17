39f99e4dbc39ad7128ab97fe6f2d8bf0d7e03800
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2026-02-17 15:35:02 +0100

    Added a "add one" button
    Added a "add one" button so that you don't need to go back to the product page to add another one

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ function drawCart() {
   cartBox.innerHTML = `
     <img src="${productImage}" alt="${productName}" style="width: 20%; display: block; margin: 0 auto 10px;">
     ${productName} x ${qty} = $${itemTotal.toFixed(2)}<br>
+    <button id="add-one-btn" class="clear-cart">Add one</button>
     <button id="remove-one-btn" class="clear-cart">Remove one</button><br>
     Cart Total: <span style="font-size: 2em;">$${itemTotal.toFixed(2)}</span>
   `;
@@ -30,6 +31,16 @@ function drawCart() {
   if (clearBtn) clearBtn.style.display = "inline-block";
 
   const removeOneBtn = document.getElementById("remove-one-btn");
+  const addOneBtn = document.getElementById("add-one-btn");
+
+  if (addOneBtn) {
+    addOneBtn.onclick = function () {
+      const currentQty = Number(localStorage.getItem(qtyKey) || "0");
+      localStorage.setItem(qtyKey, String(currentQty + 1));
+      drawCart();
+    };
+  }
+
   if (removeOneBtn) {
     removeOneBtn.onclick = function () {
       const currentQty = Number(localStorage.getItem(qtyKey) || "0");
